ef98f540d619acc01ab9c03628e3229d5a2b1eac
Mihalis <mihalisfthenos@icloud.com>
2018-04-06 19:28:20 -0700

    commented out superfluous method observer

--- common/models/industry.js
+++ common/models/industry.js
@@ -2,4 +2,28 @@
 
 module.exports = function(Industry) {
 
+	// function updateOtherCollection(collectionName, whereCondition, newData){
+	// 	var app = require('../../server/server');
+	// 	var db = app.dataSources.db;
+	// 	var collection = db.connector.collection(collectionName);		
+
+	// 	collection.update(
+	// 		whereCondition,
+	// 		{ $set: newData },
+	// 		{ multi: true }
+	// 	)
+	// }
+
+	// Industry.observe('after save', function(ctx, next) {
+	// 	if ((ctx.isNewInstance === undefined ) && (ctx.info.count > 0)) {
+	// 		// Need to update some other collections
+	// 		var where = ctx.where
+	// 		var data = ctx.data		
+	// 		updateOtherCollection('Cryptocurrency', where, data)
+	// 		console.log('updated other collections')	
+	// 	}	
+
+	// 	next()
+	// })
+
 };
