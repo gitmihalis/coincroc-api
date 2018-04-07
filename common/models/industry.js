e8fdf246a9c4502e7a758cb23ac2aaa1b3a94025
Mihalis <mihalisfthenos@icloud.com>
2018-04-07 16:27:34 -0700

    remove Industry observer

--- common/models/industry.js
+++ common/models/industry.js
@@ -2,28 +2,32 @@
 
 module.exports = function(Industry) {
 
-	// function updateOtherCollection(collectionName, whereCondition, newData){
+	// function updateOtherCollection(collectionName, newData){
 	// 	var app = require('../../server/server');
 	// 	var db = app.dataSources.db;
 	// 	var collection = db.connector.collection(collectionName);		
 
 	// 	collection.update(
-	// 		whereCondition,
-	// 		{ $set: newData },
-	// 		{ multi: true }
+	// 		{name: "Bitcoin"},
+	// 		{$set: { "industries.$.name": newData.name } },
+	// 		{multi: true},
+	// 		 { arrayFilters: [ {"industries.$": {name: "Open-source"} } ]},
+	// 		 function(err) { 
+	// 		 	if (err){
+	// 		 		return console.log(err);
+	// 		 	}
+	// 			console.log('updated other collections')	
+	// 		}
 	// 	)
 	// }
 
-	// Industry.observe('after save', function(ctx, next) {
-	// 	if ((ctx.isNewInstance === undefined ) && (ctx.info.count > 0)) {
-	// 		// Need to update some other collections
-	// 		var where = ctx.where
-	// 		var data = ctx.data		
-	// 		updateOtherCollection('Cryptocurrency', where, data)
-	// 		console.log('updated other collections')	
-	// 	}	
-
-	// 	next()
-	// })
+	Industry.observe('after save', function(ctx, next) {
+		if ((ctx.isNewInstance === undefined )) {
+			// Need to update some other collections
+			console.log('You need to update other collections')
+			// updateOtherCollection('Cryptocurrency', ctx.data)
+		}	
+		next()
+	})
 
 };
